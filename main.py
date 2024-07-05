aede6f90253a5e00a0b852a007de376e318ed88e
Warren <boweichang@berkeley.edu>
2024-07-04 20:11:40 -0700

    updates on main and gui

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
-from src.gui import RubiksCubeGUI
-import tkinter as tk
-
-def main():
-    root = tk.Tk()
-    app = RubiksCubeGUI(root)
-    root.mainloop()
+from tkinter import *
+from tkinter import ttk
+import solver
 
 if __name__ == "__main__":
-    main()
+    gui = Tk()
+    gui.title("Rubik Cube")
+    gui.geometry("1366x768")
+    ui = ttk.Notebook(gui)
+    uiLeft = ttk.Frame(ui)
+    uiRight  = ttk.Frame(ui)
+    solver.main(uiLeft)
+    frame = Frame(uiRight ,width = 950, height = 670)
+    frame.place(relx=0.5, rely=0.5,anchor = "center")
+    ui.add(uiLeft,text = "Solver")
+    ui.add(uiRight ,text = "Timer")
+    ui.pack()
+    gui.mainloop()
