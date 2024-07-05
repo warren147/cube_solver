d217b7da02abf0209298453c219fccd7df9bd0f8
Warren <boweichang@berkeley.edu>
2024-07-04 19:53:27 -0700

    updates on gui

--- src/solver.py
+++ src/solver.py
@@ -3,45 +3,45 @@ import moves
 import cube
 import kociemba         
 
-def show(side):
-    surr = {
+def showSide(cubeSide):
+    surrounding = {
         0:"2435",
         1:"2534",
         2:"1405",
         3:"0415",
         4:"2130",
         5:"2031",
-    }.get(side,dispcube)
+    }.get(cubeSide,dispcube)
 
-    side = cols[side]
-    face = {
-        cols[0]:cube.green,
-        cols[1]:cube.blue,
-        cols[2]:cube.white,
-        cols[3]:cube.yellow,
-        cols[4]:cube.red,
-        cols[5]:cube.orange,
-    }.get(side)
+    cubeSide = columns[cubeSide]
+    cubeFace = {
+        columns[0]:cube.greenSide,
+        columns[1]:cube.blueSide,
+        columns[2]:cube.whiteSide,
+        columns[3]:cube.yellowSide,
+        columns[4]:cube.redSide,
+        columns[5]:cube.orangeSide,
+    }.get(cubeSide)
     
     n=0
     for i in range(9):
-        colour = {
-            "g":cols[0],
-            "b":cols[1],
-            "w":cols[2],
-            "y":cols[3],
-            "r":cols[4],
-            "o":cols[5],
-        }.get(face[int(n/3)][int(n%3)])
+        colors = {
+            "g":columns[0],
+            "b":columns[1],
+            "w":columns[2],
+            "y":columns[3],
+            "r":columns[4],
+            "o":columns[5],
+        }.get(cubeFace[int(n/3)][int(n%3)])
         
-        buts[i].config(bg=colour,state="disabled")
-        buts[i].grid(row=int(n/3),column=int(n%3),padx=2,pady=2)
+        buttons[i].config(bg=colors,state="disabled")
+        buttons[i].grid(row=int(n/3),column=int(n%3),padx=2,pady=2)
         n+=1
 
-    Button(dispcube,width=6,height=3,bg=cols[int(surr[0])],state=DISABLED).grid(row=0,column=2,pady = 40)  #top
-    Button(dispcube,width=6,height=3,bg=cols[int(surr[1])],state=DISABLED).grid(row=2,column=4,padx = 40)  #right
-    Button(dispcube,width=6,height=3,bg=cols[int(surr[2])],state=DISABLED).grid(row=4,column=2,pady = 40)  #bottom
-    Button(dispcube,width=6,height=3,bg=cols[int(surr[3])],state=DISABLED).grid(row=2,column=0,padx = 40)  #left
+    Button(dispcube,width=6,height=3,bg=columns[int(surrounding[0])],state=DISABLED).grid(row=0,column=2,pady = 40)  #top
+    Button(dispcube,width=6,height=3,bg=columns[int(surrounding[1])],state=DISABLED).grid(row=2,column=4,padx = 40)  #right
+    Button(dispcube,width=6,height=3,bg=columns[int(surrounding[2])],state=DISABLED).grid(row=4,column=2,pady = 40)  #bottom
+    Button(dispcube,width=6,height=3,bg=columns[int(surrounding[3])],state=DISABLED).grid(row=2,column=0,padx = 40)  #left
 
 def solve(opt):
     opt.config(state = "normal")
@@ -64,16 +64,16 @@ def reset(opt,mode):
     opt.config(state = "readonly")
     for i in range(3):
         for j in range(3):
-            cube.green[i][j] = Cols[0]
+            cube.greenSide[i][j] = Cols[0]
     for i in range(3):
         for j in range(3):
-            cube.blue[i][j] = Cols[1]
+            cube.blueSide[i][j] = Cols[1]
     for i in range(3):
         for j in range(3):
-            cube.white[i][j] = Cols[2]
+            cube.whiteSide[i][j] = Cols[2]
     for i in range(3):
         for j in range(3):
-            cube.yellow[i][j] = Cols[3]
+            cube.yellowSide[i][j] = Cols[3]
     for i in range(3):
         for j in range(3):
             cube.red[i][j] = Cols[4]
@@ -81,70 +81,70 @@ def reset(opt,mode):
         for j in range(3):
             cube.orange[i][j] = Cols[5]
     if not mode.get():
-        show(0)
+        showSide(0)
     else:
-        show(0)
+        showSide(0)
         edit()
 
 def browse(mode):
     if not mode.get():
-        for i in buts:
+        for i in buttons:
             i.config(state="disabled")
     for i in selectors:
         i.config(text = "")
 
-def selected_colour(mode,c):
+def selected_colors(mode,c):
     browse(mode)
     global selected
     selected = c
     if not mode.get():
-        show(selected)
+        showSide(selected)
     else:
         selectors[c].config(text = "~")
 
 def edit():
-    buts[0].config(state="normal",command=lambda:edit_colour(0))
-    buts[1].config(state="normal",command=lambda:edit_colour(1))
-    buts[2].config(state="normal",command=lambda:edit_colour(2))
-    buts[3].config(state="normal",command=lambda:edit_colour(3))
-    buts[5].config(state="normal",command=lambda:edit_colour(5))
-    buts[6].config(state="normal",command=lambda:edit_colour(6))
-    buts[7].config(state="normal",command=lambda:edit_colour(7))
-    buts[8].config(state="normal",command=lambda:edit_colour(8))
+    buttons[0].config(state="normal",command=lambda:edit_colors(0))
+    buttons[1].config(state="normal",command=lambda:edit_colors(1))
+    buttons[2].config(state="normal",command=lambda:edit_colors(2))
+    buttons[3].config(state="normal",command=lambda:edit_colors(3))
+    buttons[5].config(state="normal",command=lambda:edit_colors(5))
+    buttons[6].config(state="normal",command=lambda:edit_colors(6))
+    buttons[7].config(state="normal",command=lambda:edit_colors(7))
+    buttons[8].config(state="normal",command=lambda:edit_colors(8))
     
 
-def edit_colour(i):
-    cur = buts[4].cget('bg')
-    face = {
-        cols[0]:cube.green,
-        cols[1]:cube.blue,
-        cols[2]:cube.white,
-        cols[3]:cube.yellow,
-        cols[4]:cube.red,
-        cols[5]:cube.orange,
+def edit_colors(i):
+    cur = buttons[4].cget('bg')
+    cubeFace = {
+        columns[0]:cube.greenSide,
+        columns[1]:cube.blueSide,
+        columns[2]:cube.whiteSide,
+        columns[3]:cube.yellowSide,
+        columns[4]:cube.redSide,
+        columns[5]:cube.orangeSide,
     }.get(cur)
-    face[int(i/3)][int(i%3)] = Cols[selected]
-    buts[i].config(bg=cols[selected])
+    cubeFace[int(i/3)][int(i%3)] = Cols[selected]
+    buttons[i].config(bg=columns[selected])
 
 
 def current_state():
     state = ""
-    for i in cube.white:
+    for i in cube.whiteSide:
         for j in i:
             state+=selecttext(j)
-    for i in cube.red:
+    for i in cube.redSide:
         for j in i:
             state+=selecttext(j)
-    for i in cube.green:
+    for i in cube.greenSide:
         for j in i:
             state+=selecttext(j)
-    for i in cube.yellow:
+    for i in cube.yellowSide:
         for j in i:
             state+=selecttext(j)
-    for i in cube.orange:
+    for i in cube.orangeSide:
         for j in i:
             state+=selecttext(j)
-    for i in cube.blue:
+    for i in cube.blueSide:
         for j in i:
             state+=selecttext(j)
     return state
@@ -161,15 +161,15 @@ def selecttext(c):
     return f
 
 def main(root):
-    global cols,Cols,selected,buts,selectors,dispcube
-    cols = ["green","blue","white","yellow","red","orange"]
+    global columns,Cols,selected,buttons,selectors,dispcube
+    columns = ["green","blue","white","yellow","red","orange"]
     Cols = ["g","b","w","y","r","o"]
     selected = 0
 
     title = LabelFrame(root,padx = 300)
     dispcube = LabelFrame(root,padx = 10,pady = 15,text = "Current State Of Cube")
     modeSel = LabelFrame(root,padx = 60, pady = 10, text = "Select Mode")
-    colourSel = LabelFrame(root,padx = 35,pady = 10,text = "Pick A Colour")
+    colorsSel = LabelFrame(root,padx = 35,pady = 10,text = "Pick A Colour")
     scramble = LabelFrame(root,text = "Scramble(Green Front White Top)")
     options = LabelFrame(root,text = "Options",padx = 80,pady = 10)
     output = LabelFrame(root,padx = 2, pady = 2,text = "Solution(Green Front White Top)")
@@ -177,7 +177,7 @@ def main(root):
     title.grid(row = 0, column = 0, columnspan=2,padx = 10,pady = 10)
     dispcube.grid(row = 1, column = 0, rowspan = 4,padx = 10)
     modeSel.grid(row = 1, column = 1,padx = 10)
-    colourSel.grid(row = 2, column = 1,padx = 10,pady = 5)
+    colorsSel.grid(row = 2, column = 1,padx = 10,pady = 5)
     scramble.grid(row = 3, column = 1)
     options.grid(row = 4, column = 1,padx = 10)
     output.grid(row = 5, column = 0,padx = 10,pady = 10)
@@ -189,12 +189,12 @@ def main(root):
     Radiobutton(modeSel, text = "Edit Side", variable = mode, value = 1,command = edit).pack(anchor = W)
 
     selectors = [
-        Button(colourSel,width=6,height=2,bg=cols[0],command=lambda:selected_colour(mode,0)),
-        Button(colourSel,width=6,height=2,bg=cols[1],command=lambda:selected_colour(mode,1)),
-        Button(colourSel,width=6,height=2,bg=cols[2],command=lambda:selected_colour(mode,2)),
-        Button(colourSel,width=6,height=2,bg=cols[3],command=lambda:selected_colour(mode,3)),
-        Button(colourSel,width=6,height=2,bg=cols[4],command=lambda:selected_colour(mode,4)),
-        Button(colourSel,width=6,height=2,bg=cols[5],command=lambda:selected_colour(mode,5)),
+        Button(colorsSel,width=6,height=2,bg=columns[0],command=lambda:selected_colors(mode,0)),
+        Button(colorsSel,width=6,height=2,bg=columns[1],command=lambda:selected_colors(mode,1)),
+        Button(colorsSel,width=6,height=2,bg=columns[2],command=lambda:selected_colors(mode,2)),
+        Button(colorsSel,width=6,height=2,bg=columns[3],command=lambda:selected_colors(mode,3)),
+        Button(colorsSel,width=6,height=2,bg=columns[4],command=lambda:selected_colors(mode,4)),
+        Button(colorsSel,width=6,height=2,bg=columns[5],command=lambda:selected_colors(mode,5)),
     ]
 
     selectors[0].grid(row=0,column=0,padx=5,pady=2)
@@ -204,24 +204,24 @@ def main(root):
     selectors[4].grid(row=2,column=0,padx=5,pady=2)
     selectors[5].grid(row=2,column=1,padx=5,pady=2)
 
-    Button(scramble,width=2,text = "R",command=lambda: [moves.R(),show(selected)]).grid(row = 0, column = 0)
-    Button(scramble,width=2,text = "L",command=lambda: [moves.L(),show(selected)]).grid(row = 0, column = 1)
-    Button(scramble,width=2,text = "F",command=lambda: [moves.F(),show(selected)]).grid(row = 0, column = 2)
-    Button(scramble,width=2,text = "B",command=lambda: [moves.B(),show(selected)]).grid(row = 0, column = 3)
-    Button(scramble,width=2,text = "U",command=lambda: [moves.U(),show(selected)]).grid(row = 0, column = 4)
-    Button(scramble,width=2,text = "D",command=lambda: [moves.D(),show(selected)]).grid(row = 0, column = 5)
-    Button(scramble,width=2,text = "R'",command=lambda: [moves.r(),show(selected)]).grid(row = 1, column = 0)
-    Button(scramble,width=2,text = "L'",command=lambda: [moves.l(),show(selected)]).grid(row = 1, column = 1)
-    Button(scramble,width=2,text = "F'",command=lambda: [moves.f(),show(selected)]).grid(row = 1, column = 2)
-    Button(scramble,width=2,text = "B'",command=lambda: [moves.b(),show(selected)]).grid(row = 1, column = 3)
-    Button(scramble,width=2,text = "U'",command=lambda: [moves.u(),show(selected)]).grid(row = 1, column = 4)
-    Button(scramble,width=2,text = "D'",command=lambda: [moves.d(),show(selected)]).grid(row = 1, column = 5)
-    Button(scramble,width=2,text = "R2",command=lambda: [moves.R2(),show(selected)]).grid(row = 2, column = 0)
-    Button(scramble,width=2,text = "L2",command=lambda: [moves.L2(),show(selected)]).grid(row = 2, column = 1)
-    Button(scramble,width=2,text = "F2",command=lambda: [moves.F2(),show(selected)]).grid(row = 2, column = 2)
-    Button(scramble,width=2,text = "B2",command=lambda: [moves.B2(),show(selected)]).grid(row = 2, column = 3)
-    Button(scramble,width=2,text = "U2",command=lambda: [moves.U2(),show(selected)]).grid(row = 2, column = 4)
-    Button(scramble,width=2,text = "D2",command=lambda: [moves.D2(),show(selected)]).grid(row = 2, column = 5)
+    Button(scramble,width=2,text = "R",command=lambda: [moves.R(),showSide(selected)]).grid(row = 0, column = 0)
+    Button(scramble,width=2,text = "L",command=lambda: [moves.L(),showSide(selected)]).grid(row = 0, column = 1)
+    Button(scramble,width=2,text = "F",command=lambda: [moves.F(),showSide(selected)]).grid(row = 0, column = 2)
+    Button(scramble,width=2,text = "B",command=lambda: [moves.B(),showSide(selected)]).grid(row = 0, column = 3)
+    Button(scramble,width=2,text = "U",command=lambda: [moves.U(),showSide(selected)]).grid(row = 0, column = 4)
+    Button(scramble,width=2,text = "D",command=lambda: [moves.D(),showSide(selected)]).grid(row = 0, column = 5)
+    Button(scramble,width=2,text = "R'",command=lambda: [moves.r(),showSide(selected)]).grid(row = 1, column = 0)
+    Button(scramble,width=2,text = "L'",command=lambda: [moves.l(),showSide(selected)]).grid(row = 1, column = 1)
+    Button(scramble,width=2,text = "F'",command=lambda: [moves.f(),showSide(selected)]).grid(row = 1, column = 2)
+    Button(scramble,width=2,text = "B'",command=lambda: [moves.b(),showSide(selected)]).grid(row = 1, column = 3)
+    Button(scramble,width=2,text = "U'",command=lambda: [moves.u(),showSide(selected)]).grid(row = 1, column = 4)
+    Button(scramble,width=2,text = "D'",command=lambda: [moves.d(),showSide(selected)]).grid(row = 1, column = 5)
+    Button(scramble,width=2,text = "R2",command=lambda: [moves.R2(),showSide(selected)]).grid(row = 2, column = 0)
+    Button(scramble,width=2,text = "L2",command=lambda: [moves.L2(),showSide(selected)]).grid(row = 2, column = 1)
+    Button(scramble,width=2,text = "F2",command=lambda: [moves.F2(),showSide(selected)]).grid(row = 2, column = 2)
+    Button(scramble,width=2,text = "B2",command=lambda: [moves.B2(),showSide(selected)]).grid(row = 2, column = 3)
+    Button(scramble,width=2,text = "U2",command=lambda: [moves.U2(),showSide(selected)]).grid(row = 2, column = 4)
+    Button(scramble,width=2,text = "D2",command=lambda: [moves.D2(),showSide(selected)]).grid(row = 2, column = 5)
 
     Button(options,width=10,text="RESET CUBE",command=lambda:reset(opt,mode)).grid(row=0,column=0)
     Button(options,width=17,text="GENERATE SOLUTION",command=lambda:solve(opt)).grid(row=1,column=0)
@@ -234,8 +234,8 @@ def main(root):
 
     holder = LabelFrame(dispcube,bg="black")
     holder.grid(row = 1,column = 1, rowspan = 3, columnspan = 3)
-    
-    buts = [
+
+    buttons = [
             Button(holder,width=6,height=3),
             Button(holder,width=6,height=3),
             Button(holder,width=6,height=3),
@@ -246,7 +246,7 @@ def main(root):
             Button(holder,width=6,height=3),
             Button(holder,width=6,height=3)
         ]
-    show(selected)
+    showSide(selected)
 
 if __name__ == "__main__":
     root = Tk()
